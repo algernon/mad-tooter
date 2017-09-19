c446217c1c60b23634068e8e8e2695e651ff55e9
Gergely Nagy <mastodon@gergo.csillger.hu>
2017-09-19 16:20:13 +0200

    Timeline
    Signed-off-by: Gergely Nagy <mastodon@gergo.csillger.hu>

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import List, { ListItem } from 'material-ui/List';
+import { LinearProgress } from 'material-ui/Progress';
 
-import TootAPICard from './toot/APICard';
+import TootCard from './toot/Card';
 import TootFrame from './frame/Frame';
+
+import { AuthToken } from '../config/authToken';
+
+import axios from 'axios';
 
 const styles = theme => ({
     list: {
@@ -14,21 +19,51 @@ const styles = theme => ({
     },
 });
 
+class Timeline extends React.Component {
+    state = {
+        axios: axios.create({
+            baseURL: 'https://trunk.mad-scientist.club/api/v1',
+            headers: {"Authorization": "Bearer " + AuthToken},
+        }),
+        timeline: [],
+    };
+
+    componentDidMount () {
+        this.state.axios.get("/timelines/home")
+            .then((response) => {
+                this.setState({timeline: response.data})
+            });
+    }
+
+    render () {
+        if (this.state.timeline.length === 0)
+            return (
+                <LinearProgress color="accent" mode="query" />
+            );
+
+        return (
+            <List className={this.props.classes.list} width="100%" dense>
+              {this.state.timeline.map(toot => {
+                  return (
+                      <ListItem>
+                            <TootCard toot={toot} />
+                      </ListItem>
+                  );
+              })}
+            </List>
+        );
+    }
+}
+Timeline.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+Timeline = withStyles(styles)(Timeline);
+
 class MadTooter extends React.Component {
     render() {
         return (
             <TootFrame>
-              <List className={this.props.classes.list} width="100%" dense>
-                <ListItem>
-                  <TootAPICard url="https://trunk.mad-scientist.club/api/v1/statuses/3849086" />
-                </ListItem>
-                <ListItem>
-                  <TootAPICard url="https://trunk.mad-scientist.club/api/v1/statuses/3842390" />
-                </ListItem>
-                <ListItem>
-                  <TootAPICard url="https://trunk.mad-scientist.club/api/v1/statuses/3831242" />
-                </ListItem>
-              </List>
+              <Timeline />
             </TootFrame>
         );
     }
